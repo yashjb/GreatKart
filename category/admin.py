cb811c59bb41fcb45c244176fda30d2c710feb46
Yash Janeshwar <y.janeshwar@uvation.com>
2024-02-18 11:29:32 +0530

    Pre-populate Category Slug for dj admin

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Category
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)} # to prepopulate category field while creating new category from Admin dashboard 
+    list_display = ('category_name', 'slug',)
+
+admin.site.register(Category, CategoryAdmin)
